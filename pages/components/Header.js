f39cce8352621dea2d03227cf066d4792da774dc
takumi <frontline4701@gmail.com>
2022-04-24 10:48:48 +0900

    menus

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,6 +1,6 @@
 
 
-function Header(props) {
+function Header({ title, menu, setMenu }) {
 
 
 
@@ -17,7 +17,7 @@ function Header(props) {
       bg-red-200 absolute w-full bottom-0 h-20 flex justify-around items-center font-bold shadow-md
       md:top-0 md:justify-between
       ">
-        <h1 className="hidden md:block pl-20">{props.title}</h1>
+        <h1 className="hidden md:block pl-20">{title}</h1>
 
         <div className="flex justify-around w-full md:mr-20 md:w-[45vw]">
           <p className="">test</p>
---
+++ pages/components/Menu1.js
@@ -0,0 +1,6 @@
+function Menu1() {
+  return (
+    <div>Menu1das</div>
+  )
+}
+export default Menu1
